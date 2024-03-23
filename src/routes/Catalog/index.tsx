71248ab90bf2d07ddfd372d30ee26e378f9ee4d6
Gustavo Moreno <gmorenosmail@gmail.com>
2024-03-23 18:03:17 -0300

    Search bar

--- src/routes/Catalog/index.tsx
+++ src/routes/Catalog/index.tsx
@@ -1,12 +1,23 @@
-import Header from '../../components/Header';
-import './styles.css';
+import "./styles.css";
+import Header from "../../components/Header";
+import SearchBar from "../../components/SearchBar";
 
 export default function Catalog() {
+  return (
+    <>
+      <Header />
+      <main className="ct-catalog-main">
+        <section id="ct-catalog-section" className="ct-container">
+            <div className="ct-search-baricontainer">
+                <SearchBar />
+            </div>
 
-    return (
+            <div className="ct-catalog-cards-container">
+                <div>Teste</div>
+            </div>
 
-        <Header />
-
-    );
-
+        </section>
+      </main>
+    </>
+  );
 }
